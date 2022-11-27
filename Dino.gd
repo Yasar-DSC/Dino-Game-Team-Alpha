9da1be82d212001b1833a000c326c108bbbd45a6
berenpolat <polatberen24@gmail.com>
2022-11-27 19:33:46 +0300

    dino

--- Dino.gd
+++ Dino.gd
@@ -2,9 +2,8 @@ extends Area2D
 
 var velocity =Vector2.ZERO
 var jump_velocity = Vector2.UP* 600
-var gravity = Vector2.DOWN * 2000
-var gravity_l= Vector2.DOWN * 4000
 
+var gravity_l= 4000
 
 func _process(delta):
 	if Input.is_action_just_pressed("jump"):
@@ -16,3 +15,17 @@ func _process(delta):
 			velocity += gravity * delta	
 		
 		move_and_slide(velocity, Vector2.UP)
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
