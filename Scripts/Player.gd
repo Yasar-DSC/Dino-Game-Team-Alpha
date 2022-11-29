f9f0881902045d4ceaa94f1e8a2fccd9a088d780
Mehmet Yıldırım <31160903+SilentRustle@users.noreply.github.com>
2022-11-30 00:34:38 +0300

    Replay, random obstacles and many more!

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,23 +1,25 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO
-var jump_velocity = Vector2.UP * 600
-var gravity = Vector2.DOWN * 2000
-var down_pressed_gravity = Vector2.DOWN * 4000
+var jump_velocity = 800
+var gravity = 2000
+var down_pressed_gravity = 4000
 var start = false
 
 func _physics_process(delta):
 #	if start == false:
 #		$AnimationPlayer.play("idle")
 #		return
-	
-	if is_on_floor() and Input.is_action_pressed("ui_up"):
-		velocity = jump_velocity
-	if !is_on_floor():
-		if Input.is_action_pressed("ui_down"):
-			velocity += down_pressed_gravity * delta
-		else:
-			velocity += gravity * delta
+
+	if start == true:
+		if is_on_floor() and Input.is_action_pressed("ui_up"):
+			velocity.y = -jump_velocity
+		if !is_on_floor():
+			if Input.is_action_pressed("ui_down"):
+				velocity.y += down_pressed_gravity * delta
+			else:
+				velocity.y += gravity * delta
+	move_and_slide(velocity, Vector2.UP)
 
 
 # Varsa animasyonlar yeri
